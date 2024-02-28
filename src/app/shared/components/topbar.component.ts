cd69e5daf1af8fb36dc89bc35ff5503d53e4d01f
Popa Serban Alexandru <serban.popa@outlook.com>
2024-02-28 13:35:37 +0200

    Add primeng library and import ButtonModule

---
+++ src/app/shared/components/topbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'lh-topbar',
+    templateUrl: './topbar.component.html',
+    standalone: true,
+})
+export class TopbarComponent { 
+    
+}
