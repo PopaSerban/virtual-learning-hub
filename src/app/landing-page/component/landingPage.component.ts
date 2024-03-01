a0f00839d0c77b590483e82344ee695982a98000
Popa Serban Alexandru <serban.popa@outlook.com>
2024-03-01 13:20:13 +0200

    Refactor landing page component and add landing layout component

--- src/app/landing-page/component/landingPage.component.ts
+++ src/app/landing-page/component/landingPage.component.ts
@@ -3,12 +3,13 @@ import { TopbarComponent } from "../../shared/components/topbar/topbar.component
 import { menuItems } from "../../shared/components/topbar/models/menu-items.model";
 import { MenuItem } from "primeng/api";
 import {PanelModule} from 'primeng/panel';
+import { LandingLayoutComponent } from "../../shared/components/landingLayout/landingLayout.component";
 
 @Component({
     selector: 'lh-landing-page',
     templateUrl: './landingPage.component.html',
     standalone: true,
-    imports: [TopbarComponent, PanelModule],
+    imports: [TopbarComponent, PanelModule, LandingLayoutComponent]
 })
 export class LandingPageComponent {
      menuItems: MenuItem[] = menuItems;
--- src/app/shared/components/landingLayout/landingLayout.component.ts
+++ src/app/shared/components/landingLayout/landingLayout.component.ts
@@ -1,10 +1,39 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'lh-landing-layout',
-    templateUrl: './landingLayout.component.html',
-    standalone: true,
+  selector: 'lh-landing-layout',
+  standalone: true,
+  template: `
+    <div class="container">
+      <div class="main-layout">
+        <nav class="menu-layout">
+          <ng-content select="[menu]"></ng-content>
+        </nav>
+        <div class="body">
+          <ng-content select="[body]"></ng-content>
+        </div>
+        <footer class="footer-layout">
+          <ng-content select="[footer]"></ng-content>
+        </footer>
+      </div>
+    </div>
+  `,
+  styles: `
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    .main-layout {
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+        height: 100%;
+    }
+    .menu-layout {
+        margin: 0 3rem;
+    }
+    `,
 })
-export class LandingLayoutComponent {
-    
-}
+export class LandingLayoutComponent {}
