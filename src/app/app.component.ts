fd82c2c62fe74d53f9ea0651e5a25f0b8936ba3b
Popa Serban Alexandru <serban.popa@outlook.com>
2024-03-01 13:08:46 +0200

    Update imports and fix styling in app.component.ts, app.config.ts, landingPage.component.html, and topbar.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TopbarComponent } from './shared/components/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,TopbarComponent],
+  imports: [CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/landing-page/component/landingPage.component.ts
+++ src/app/landing-page/component/landingPage.component.ts
@@ -2,12 +2,13 @@ import { Component } from "@angular/core";
 import { TopbarComponent } from "../../shared/components/topbar/topbar.component";
 import { menuItems } from "../../shared/components/topbar/models/menu-items.model";
 import { MenuItem } from "primeng/api";
+import {PanelModule} from 'primeng/panel';
 
 @Component({
     selector: 'lh-landing-page',
     templateUrl: './landingPage.component.html',
     standalone: true,
-    imports: [TopbarComponent]
+    imports: [TopbarComponent, PanelModule],
 })
 export class LandingPageComponent {
      menuItems: MenuItem[] = menuItems;
